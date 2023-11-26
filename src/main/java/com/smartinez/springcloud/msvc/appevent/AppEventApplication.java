e1ec7e5d0c23d378e910d1f884ebdd130eb3f2e2
SalvadorSimonDev <salvador.simon.88@gmail.com>
2023-11-26 13:00:39 +0100

    creación de estructura de proyecto con micro-servicio msvc-user

---
+++ src/main/java/com/smartinez/springcloud/msvc/appevent/AppEventApplication.java
@@ -0,0 +1,13 @@
+package com.smartinez.springcloud.msvc.appevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartinez/springcloud/msvc/appevent/AppEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartinez.springcloud.msvc.appevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
